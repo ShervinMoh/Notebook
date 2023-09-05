c985e75ed7f7bf5b3c3525f18e9e2529b4cce291
Shervin.Moh <shervin.mohammadifar@gmail.com>
2023-09-05 19:03:59 +0330

    Added entery function for stores title and text

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import json
 class Notebook:
     def __init__(self):
         self.entries = []
+
+    # We create a dictionary that stores the titles and text inside itself
+    def add_entry(self, title, user_text):
+        entry = {
+            'title': title,
+            'user_text': user_text
+        }
+        self.entries.append(entry)
