93e4a49022df7ebae7d43fe052d63ede17ea7ac1
Shervin.Moh <shervin.mohammadifar@gmail.com>
2023-09-09 13:17:59 +0330

    remove current menu

--- main.py
+++ main.py
@@ -52,23 +52,3 @@ class Notebook:
         self.entries.append(entry)
 
 notebook = Notebook()
-
-menu = input("""What do you want? 
-            If you need to add data, type 'add data'
-            If you need to clear notebook, type 'clear'\n""")
-
-if menu == "add data":
-    # Added input for getting values from the user
-    num_entries = int(input("Enter the number of entries you want to add: "))
-
-    for i in range(num_entries):
-        title = input("Enter the entry title: ")
-        user_text = input("Enter the user text: ")
-        notebook.add_entry(i, title, user_text)
-
-    # Execute the code
-    notebook.save_to_json("savedata.json")
-    notebook.display_entries()
-
-else:
-    print("Check the command you have chosen")
